3a2807755293cd4412f25cb6dbff08fc9d8d4e3e
glenda-mahayana <88453664+glenda-mahayana@users.noreply.github.com>
2026-03-09 07:26:43 -0300

    test

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    baseUrl: "https://automationexercise.com/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,5 +7,26 @@ describe('Test Case 01', () => {
     cy.get('h1').contains('Automation');
   })
 
-})
+  it('Usuario e senha invalidos', () => {
+    cy.visit('/');
+    cy.get('div.shop-menu').contains('Login').click();
+    cy.contains('Login to your account');
+   // cy.get('.login-form').find('input[name = "email"]');
+    cy.get('[data-qa="login-email"]').type('testqa@test.com');
+    cy.get('[data-qa="login-password"]').type('123456');
+    cy.get('[data-qa="login-button"').contains('Login').click();
+    cy.contains('Your email or password is incorrect!');
 
+  });
+
+  it.only('Validação de API usando intercept ', () => {
+    cy.request('GET', 'api/productsList').should((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).not.to.be.empty;
+      let body = JSON.parse(response.body);
+      expect(body.products).to.be.an('array');
+      expect(body.products).to.have.length.above(1);
+    });
+  });
+
+})
