06ec6b399dc12c95b5bb0bb17070fe09df88d831
glenda-mahayana <gmahayana@tech6group.com>
2026-05-19 07:06:03 -0300

    test 06

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: 'go5hz9',
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: 'https://restful-booker.herokuapp.com',
