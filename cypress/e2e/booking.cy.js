ae74f661b77661e8a071815c962e136beaee6369
glenda-mahayana <gmahayana@tech6group.com>
2026-05-20 07:22:02 -0300

    test 07

--- cypress/e2e/booking.cy.js
+++ cypress/e2e/booking.cy.js
@@ -12,7 +12,7 @@ describe('Test Suit - Booking API Testing', () => {
         })
     });
 
-    it.only('2 - Get booking by id First Name', () => {
+    it('2 - Get booking by id First Name', () => {
         cy.request({
           method: 'GET',
           url:'/booking',
@@ -21,6 +21,25 @@ describe('Test Suit - Booking API Testing', () => {
         }).then((response) => {
             expect(response.status).to.eq(200);
             expect(response.body).to.be.an('array');
+        })
+    })
+
+    it.only('3 - Get booking by id', () => {
+        cy.request({
+            method: 'GET',
+            url:'/booking/1',
+            headers: {'Accept': 'application/json'}
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.status).to.have.property('content-type', 'application/json; charset=utf-8');
+            expect(response.body).to.be.an('object');
+            expect(response.body).to.have.property('firstname').and.to.be.a('string');
+            expect(response.body).to.have.property('lastname').and.to.be.a('string');
+            expect(response.body).to.have.property('totalprice').and.to.be.a('number');
+            expect(response.body).to.have.property('depositpaid').and.to.be.a('boolean');
+            expect(response.body).to.have.property('bookingdates').and.to.be.a('object');
+            expect(response.body.bookingdates).to.have.property('checkin').and.to.be.a('string');
+            expect(response.body.bookingdates).to.have.property('checkout').and.to.be.a('string');
         })
     })
 });
